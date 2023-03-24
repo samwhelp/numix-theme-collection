d51c8df43cf95f9f7308eafdb5fe9cb400433214
samwhelp <samwhelp@users.noreply.github.com>
2023-03-25 01:55:21 +0800

    fix

---
+++ _demo/clone.sh
@@ -0,0 +1,50 @@
+
+##
+## https://github.com/samwhelp/numix-theme-collection
+##
+
+git clone -b gh-pages git@github.com:samwhelp/numix-theme-collection.git numix-theme-collection-gh-pages
+
+cd numix-theme-collection-gh-pages
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+
+git clone -b all git@github.com:samwhelp/numix-theme-collection.git numix-theme-collection-all
+
+cd numix-theme-collection-all
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b gtk-theme-numix-blue git@github.com:samwhelp/numix-theme-collection.git numix-theme-collection-gtk-theme-numix-blue
+
+cd numix-theme-collection-gtk-theme-numix-blue
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b source git@github.com:samwhelp/numix-theme-collection.git numix-theme-collection-source
+
+cd numix-theme-collection-source
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
